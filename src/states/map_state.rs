06aa90fec3d8bf83a7dc1e39488231d88f6a6cfa
Gaël <gael.batault@arche-mc2.fr>
2024-10-16 11:55:16 +0200

    canvas

--- src/states/map_state.rs
+++ src/states/map_state.rs
@@ -7,32 +7,10 @@ pub enum Infra {
     Station,
 }
 
-impl Infra {
-    pub fn to_tile_class(&self) -> String 
-    {
-        match &self {
-            Infra::Rail => "tile--rail".to_string(),
-            Infra::Station => "tile--station".to_string(),
-        }
-    }
-}
-
-#[derive(Default, Copy, Clone, PartialEq, Store)]
-pub struct Tile {
-    pub index: usize,
-    pub infra: Option<Infra>,
-}
-
 #[derive(Default, Clone, PartialEq, Store)]
 pub struct MapState {
     pub is_init: bool,
     pub is_menu_build: bool,
     pub is_build_mode: bool,
     pub infra: Infra,
-    pub is_drag: bool,
-    pub x: i32,
-    pub y: i32,
-    pub prev_x: i32,
-    pub prev_y: i32,
-    pub tiles: Vec<Tile>,
 }
