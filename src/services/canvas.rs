3a8823fd699f79f8a2de6a23e710353ee4f67fea
Gaël <gael.batault@arche-mc2.fr>
2024-11-27 14:03:40 +0100

    Only draw visible tiles

--- src/services/canvas.rs
+++ src/services/canvas.rs
@@ -11,7 +11,7 @@ pub fn draw_map() {
     
     //log::info!("draw_map");
 
-    let tile_size = TILE_SIZE as f64;
+    let tile_size = TILE_SIZE as i32;
     let rail_size = TILE_SIZE as u32;
 
     let window = web_sys::window().unwrap();
@@ -36,13 +36,13 @@ pub fn draw_map() {
     
     ctx.clear_rect(0., 0., canvas.width() as f64, canvas.height() as f64);
 
-    let mut map_x = 0.;
-    let mut map_y = 0.;
+    let mut cam_x = 0;
+    let mut cam_y = 0;
     if let Some(camera_x) = storage::get(StorageKey::CameraX) {
-        map_x = camera_x.parse::<f64>().unwrap();
+        cam_x = camera_x.parse::<i32>().unwrap();
     }
     if let Some(camera_y) = storage::get(StorageKey::CameraY) {
-        map_y = camera_y.parse::<f64>().unwrap();
+        cam_y = camera_y.parse::<i32>().unwrap();
     }
 
     let mut rail_img = None;
@@ -54,27 +54,39 @@ pub fn draw_map() {
 
     let tiles_sto = Tiles::from_storage();
     if let Some(tiles) = tiles_sto {
+        
+        let fts = tile_size as f64;
+
         for (i, row) in tiles.data.iter().enumerate() {
             for (j, col) in row.iter().enumerate() {
-                let x = i as f64 * tile_size + map_x;
-                let y = j as f64 * tile_size + map_y;
+                let x = i as i32 * tile_size + cam_x;
+                let y = j as i32 * tile_size + cam_y;
                 
-                ctx.set_fill_style(&DIRT.into());
-                ctx.set_stroke_style(&DARK_DIRT.into());
-                ctx.fill_rect(x, y, tile_size, tile_size);
-                ctx.stroke_rect(x, y, tile_size, tile_size);
-    
-                if col == &Infra::Rail {
-
-                    if let Some(img) = rail_img.clone() {
-                       let res = ctx.put_image_data(&img, x, y);
-                       if res.is_err() {
-                            log::error!("err");
-                       }
+                // Only draw visible tiles 
+                if x >= -tile_size && x <= (vw as i32 + tile_size) 
+                    && y >= -tile_size && y <= (vh as i32 + tile_size) {
+                    
+                    let fx = x as f64;
+                    let fy = y as f64;
+                    
+                    ctx.set_fill_style(&DIRT.into());
+                    ctx.set_stroke_style(&DARK_DIRT.into());
+                    ctx.fill_rect(fx, fy, fts, fts);
+                    ctx.stroke_rect(fx, fy, fts, fts);
+        
+                    if col == &Infra::Rail {
+                        
+                        if let Some(img) = rail_img.clone() {
+                           let res = ctx.put_image_data(&img, fx, fy);
+                           if res.is_err() {
+                                log::error!("err");
+                           }
+                        }
+                        //ctx.set_fill_style(&BLACK.into());
+                        //ctx.fill_rect(x, y + 15., tile_size, 5.);
                     }
-                    //ctx.set_fill_style(&BLACK.into());
-                    //ctx.fill_rect(x, y + 15., tile_size, 5.);
                 }
+                
             }
         }
     }
